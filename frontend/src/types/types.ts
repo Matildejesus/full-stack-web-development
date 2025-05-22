7f7d6a270f89c02fd9f58284974e9c0620fce726
Matildejesus <123233626+Matildejesus@users.noreply.github.com>
2025-05-22 13:48:18 +1000

    Create types.ts

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,60 @@
+export interface Admin {
+    id: number;
+    user: User;
+}
+
+export interface Application {
+    id: number;
+    candidate: Candidate;
+    course: Course;
+    previousRole: string;
+    jobRole: string;
+    availability: Availability;
+    skills: string[];
+    academic: string;
+    selectedCount: number;
+    lecturerSelections: LecturerSelection[];
+}
+
+export interface Candidate {
+    id: number;
+    user: User;
+    applications: Application[];
+}
+
+export interface Course {
+    id: number;
+    code: string;
+    name: string;
+    lecturer: Lecturer;
+    applications: Application[];
+}
+
+export interface Lecturer {
+    id: number;
+    user: User;
+    courses: Course[];
+    lecturerSelections: LecturerSelection[];
+}
+
+export interface LecturerSelection {
+    id: number;
+    rank: number | null;
+    comment: string;
+    lecturer: Lecturer;
+    application: Application;
+   
+}
+
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    avatarUrl: string | null;
+    role: Role;
+    lecturer?: Lecturer;
+    candidate?: Candidate;
+    admin?: Admin;
+}
